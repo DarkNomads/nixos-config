9f8e3f8c63fb91ccc5fe4ed1b991c1bc7f389700
DarkNomads <ghostnomadjr@gmail.com>
2026-02-27 00:15:24 -0700

    Removed xserver.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -17,7 +17,7 @@
   users.users.darknomads = {
     isNormalUser = true;
     description = "darknomads";
-    extraGroups = [ "networkmanager" "wheel" "seat" ];
+    extraGroups = [ "networkmanager" "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDD72Bgb2WCq/DHet7Dm8phhzE2Hos0473JTRUrf9GUFH7ZcGZaFnR4Wjs64hDSThP8sOo0Is3e8HLUDRsoRYYf2V66i1Rgpfi91ljnSfyxHK+cRS59pb8hZk9TJN+Ynpqd+0M9SyTSWyaDdjoNMWJDyECsDBi3P0H1GTLS819GXrTXkPeXYivLfeImX4E3aXkmfiAJhepS6wsbupthU5eQDIgxD6yZk479BraRITyte7zAWwnSWEZtC4SNV9cw1fqYen/Nva3EcFpGJJyi+E+xAxVCIvsNvkDECoGX6gZBoQQDMe1HdTtnoBjNnqmP+pBOyNtqec3ZYykua3jl46fyLImnJNXTkMpGuZQmuCTaiFeuZ5QowsVR0087pMrLZs7ZvbibDXFZhoV3Rd669lqFLXaolyV5QN48LNfp+dWxPCL38W4l7KSxP8NWgpYK1MNXqfYSFMjs2eIjlLmBbKAr1nFkCEmc5K07C+tLyLd32dJr746lzRuXlEH5je3RskcWAr+nupgQfeTNULWcgCP9QbbRySmjxIgIePaAhZm9x5rfuVdCS9nXaAHyb+jbwAuU+ncpjW8HQqTy+0iM9ufdTKuQPnPcHmoy05LrCQcurnEtcRs35byeiH2nlFRxL9Doy3/qmS1pA0O2bwE4GJpiqfOLbM7Jaxa2+nZAdWp9KQ== ghostnomadjr@gmail.com"
     ];
@@ -54,13 +54,9 @@
   ];
 
   programs.sway.enable = true;
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" "modesetting" ];
-  services.displayManager.lemurs.enable = true;
+  programs.ssh.startAgent = true;
 
-  services.xserver.xkb.layout = "us";
   services.printing.enable = true;
-
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -69,9 +65,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-  programs.nano.enable = false;
-  programs.ssh.startAgent = true;
 
   services.openssh = {
     enable = true;
