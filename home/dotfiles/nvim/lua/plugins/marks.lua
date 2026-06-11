2cc99f7af15573d90ec51dcf241211c42a80ec01
DarkNomads <ghostnomadjr@gmail.com>
2026-06-10 19:42:51 -0600

    Fixed marks.nvim column highlight, fixed marks.nvim not refreshing after auto formatter.

--- home/dotfiles/nvim/lua/plugins/marks.lua
+++ home/dotfiles/nvim/lua/plugins/marks.lua
@@ -1,3 +1,18 @@
 vim.pack.add({ "https://github.com/chentoast/marks.nvim" })
 
 require("marks").setup()
+
+local group = vim.api.nvim_create_augroup("marks-fix-hl", {})
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  group = group,
+  callback = function()
+    vim.api.nvim_set_hl(0, "MarkSignNumHL", {})
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  callback = function(_)
+    local marks = require("marks")
+    vim.schedule(function() marks.refresh(_) end)
+  end,
+})
